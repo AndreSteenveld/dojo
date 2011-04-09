398f47f24e8ab883ce4b625ce4b7ff696daddfe0
Kris Zyp <kriszyp@gmail.com>
2011-04-09 14:32:18 -0600

    More listen tests

--- tests/listen.js
+++ tests/listen.js
@@ -110,7 +110,25 @@ doh.register("tests.listen",
 				bubbles: true,
 				cancelable: true
 			}));
-			t.is(order, [0, 1, 2, 3, 4, 5, 6]);
+			var button = div.appendChild(document.createElement("button"));
+			// make sure we are propagating natively created events too
+			listen(div, "click", function(){
+				order.push(7);
+			});
+			button.click();
+			t.is(order, [0, 1, 2, 3, 4, 5, 6, 7]);
+		},
+		function Evented(t){
+			var MyClass = dojo.declare([listen.Evented],{
+				
+			});
+			var order = [];
+			myObject = new MyClass;
+			myObject.on("custom", function(event){
+				order.push(event.a);
+			});
+			myObject.emit("custom", {a:0});
+			t.is(order, [0]);
 		}
 	]
 );
